591812b51b33b3cdf7be03e03262dfcd46c45d43
zaram-beeps <chukwumaobidivine87@gmail.com>
2025-10-05 10:18:35 +0100

    changes

--- models/user.js
+++ models/user.js
@@ -22,7 +22,7 @@ const User = sequelize.define("User", {
     },
     password: {
         type: DataTypes.STRING,
-        allowNull: false,
+        allowNull: true,
     }
 }, { 
     timestamps: true,
